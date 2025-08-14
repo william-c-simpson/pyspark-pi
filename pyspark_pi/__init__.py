027bda56edb706363da8f1a51776147d212ea3fa
william <william@localhost.localdomain>
2025-08-14 17:28:53 -0500

    Removed urllib warning suppression that was left in by mistake

--- pyspark_pi/__init__.py
+++ pyspark_pi/__init__.py
@@ -1,4 +1,3 @@
-import urllib3
 import packaging.version
 
 import pyspark
@@ -6,8 +5,6 @@ from pyspark_pi.data_source import PiDataSource
 from pyspark_pi.helpers import result_schema
 
 __all__ = ["PiDataSource", "result_schema"]
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 if packaging.version.parse(pyspark.__version__) < packaging.version.parse("4.0.0"):
     raise ImportError(
